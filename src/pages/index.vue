3776edab6b62d552a47f774cf4abbca0fa35dd47
nlebedevinc <nlebedevme@gmail.com>
2021-05-13 12:18:46 +0200

    New particles patterns

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,94 +1,93 @@
 <template>
   <div id="index">
     <Particles
-                id="tsparticles"
-                :options="{
-                    background: {
-                        color: {
-                            value: '#0d47a1'
-                        }
-                    },
-                    fpsLimit: 60,
-                    interactivity: {
-                        detectsOn: 'canvas',
-                        events: {
-                            onClick: {
-                                enable: true,
-                                mode: 'push'
-                            },
-                            onHover: {
-                                enable: true,
-                                mode: 'repulse'
-                            },
-                            resize: true
-                        },
-                        modes: {
-                            bubble: {
-                                distance: 400,
-                                duration: 2,
-                                opacity: 0.8,
-                                size: 40
-                            },
-                            push: {
-                                quantity: 4
-                            },
-                            repulse: {
-                                distance: 200,
-                                duration: 0.4
-                            }
-                        }
-                    },
-                    particles: {
-                        color: {
-                            value: '#ffffff'
-                        },
-                        links: {
-                            color: '#ffffff',
-                            distance: 150,
-                            enable: true,
-                            opacity: 0.5,
-                            width: 1
-                        },
-                        collisions: {
-                            enable: true
-                        },
-                        move: {
-                            direction: 'none',
-                            enable: true,
-                            outMode: 'bounce',
-                            random: false,
-                            speed: 6,
-                            straight: false
-                        },
-                        number: {
-                            density: {
-                                enable: true,
-                                value_area: 800
-                            },
-                            value: 80
-                        },
-                        opacity: {
-                            value: 0.5
-                        },
-                        shape: {
-                            type: 'circle'
-                        },
-                        size: {
-                            random: true,
-                            value: 5
-                        }
-                    },
-                    detectRetina: true
-                }"
-        />
-        <Card/>
+      id="tsparticles"
+      :options="{
+        background: {
+          color: {
+            value: '#2e2e2e',
+          },
+        },
+        fullScreen: {
+          enable: true,
+          zIndex: 1,
+        },
+        interactivity: {
+          events: {
+            onClick: {
+              enable: true,
+              mode: 'push',
+            },
+            onHover: {
+              enable: true,
+              mode: 'grab',
+            },
+          },
+        },
+        particles: {
+          color: {
+            value: '#ff0000',
+            animation: {
+              h: {
+                enable: true,
+                speed: 20,
+              },
+            },
+          },
+          links: {
+            color: {
+              value: '#ffffff',
+            },
+            enable: true,
+            opacity: 0.4,
+          },
+          move: {
+            enable: true,
+            path: {},
+            outModes: {
+              bottom: 'out',
+              left: 'out',
+              right: 'out',
+              top: 'out',
+            },
+          },
+          number: {
+            density: {
+              enable: true,
+            },
+            value: 80,
+          },
+          opacity: {
+            value: 0.5,
+            animation: {
+              speed: 4,
+            },
+          },
+          size: {
+            value: {
+              min: 0.5,
+              max: 4,
+            },
+            animation: {
+              speed: 1,
+            },
+          },
+          twinkle: {
+            lines: {
+              frequency: 0.1,
+            },
+          },
+        },
+      }"
+    />
+    <Card />
   </div>
 </template>
 
 <script lang="ts">
-import Vue from 'vue'
+import Vue from "vue";
 
-export default Vue.extend({})
+export default Vue.extend({});
 </script>
 
 <style>
@@ -102,16 +101,8 @@ export default Vue.extend({})
 }
 
 .title {
-  font-family:
-    'Quicksand',
-    'Source Sans Pro',
-    -apple-system,
-    BlinkMacSystemFont,
-    'Segoe UI',
-    Roboto,
-    'Helvetica Neue',
-    Arial,
-    sans-serif;
+  font-family: "Quicksand", "Source Sans Pro", -apple-system, BlinkMacSystemFont,
+    "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif;
   display: block;
   font-weight: 300;
   font-size: 100px;
@@ -142,12 +133,9 @@ export default Vue.extend({})
 
 #tsparticles {
   position: absolute;
-    top: 0;
-    left: 0;
-    right: 0;
-    bottom: 0;
-    canvas {
-      height: 99% !important;
-    }
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
 }
 </style>
