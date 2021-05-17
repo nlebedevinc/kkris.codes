d93d6d3e518861ab45f4b3c3911dd9310df1c1cd
nlebedevinc <nlebedevme@gmail.com>
2021-05-17 10:18:21 +0200

    Add right information

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -4,35 +4,35 @@
     <h2>Data Scientist</h2>
 
     <div class="card-icons">
-      <a href="https://github.com/nlebedevinc">
+      <a href="mailto:k.kalbasuik@gmail.com">
         <fa icon="envelope" class="card-icon" />
       </a>
 
-      <a href="https://github.com/nlebedevinc">
+      <a href="https://github.com/kkalbasiuk">
         <fa :icon="['fab', 'github']" class="card-icon" />
       </a>
 
-      <a href="https://github.com/nlebedevinc">
+      <a href="https://github.com/nlebedevinc" class="hide">
         <fa :icon="['fab', 'twitter']" class="card-icon" />
       </a>
 
-      <a href="https://github.com/nlebedevinc">
+      <a href="https://www.linkedin.com/in/kristina-kalbasiuk">
         <fa :icon="['fab', 'linkedin']" class="card-icon" />
       </a>
 
-      <a href="https://github.com/nlebedevinc">
+      <a href="https://github.com/nlebedevinc" class="hide">
         <fa :icon="['fab', 'medium']" class="card-icon" />
       </a>
 
-      <a href="https://github.com/nlebedevinc">
+      <a href="skype:live:a796f42f22545dbf?chat">
         <fa :icon="['fab', 'skype']" class="card-icon" />
       </a>
 
-      <a href="https://github.com/nlebedevinc">
+      <a href="https://t.me/kkkriskk">
         <fa :icon="['fab', 'telegram']" class="card-icon" />
       </a>
 
-      <a href="https://github.com/nlebedevinc">
+      <a href="https://github.com/nlebedevinc" class="hide">
         <fa :icon="['fab', 'instagram']" class="card-icon" />
       </a>
     </div>
@@ -51,8 +51,21 @@
   margin-top: 30px;
 }
 
+.hide {
+  display: none;
+}
+
 .card-icon {
   font-size: 32px;
   margin: 5px;
 }
+
+.card-icons a {
+  color: #000;
+}
+
+.card-icons a:hover {
+  color: #f0be69;
+}
+
 </style>
